5ea0f2f36e7a1ca52115af6a1d980f434fde4547
Anderson Blanco <riveraanderson2910@gmail.com>
2023-02-14 09:58:09 -0600

    ultima serie

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print('Anderson Emilio Blanco Rivera Carné: 1566323')
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
+
+    archivo = open(".gitignore")
+    archivo.close()
